afabd4fdf599d5e78a9b83a2771271b28ed167e1
PieroniJV <vitorpieroni1@gmail.com>
2022-12-02 09:47:38 +0000

    first commit

---
+++ src/components/bookItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
+
+export class BookItem extends React.Component {
+    render() {
+        return (
+            <div>
+                {/* <h4>{this.props.book.title}</h4>
+        <img src={this.props.book.thumbnailUrl}></img>
+                <h6>{this.props.book.authors[0]}</h6> */}
+
+                <Card>
+            <Card.Header>{this.props.book.title}</Card.Header>
+                    <Card.Body>
+       <blockquote className="blockquote mb-0">
+            <img src={this.props.book.cover}></img>
+            <footer >
+                {this.props.book.author}
+                    </footer>
+                        </blockquote>
+                    </Card.Body>
+                    <Link className="btn btn-primary" to={'/edit/' + this.props.book._id}>
+                        Edit
+                    </Link>
+                </Card>
+            </div>
+        );
+    }
+}
